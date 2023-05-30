3f93a13fe23cfb2a1a5c5f00628995950101273f
injin <injin33@naver.com>
2023-05-30 22:17:40 +0900

    컴포넌트 스캔

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # spring-basic
 [인프런] 스프링 핵심 원리 - 기본편
 
-주문과 할인 도메인 설계 볼 차례
+## 4. 스프링 컨테이너와 스프링 빈
+### 4.2 컨테이너에 등록된 모든 빈 조회
+- ApplicationContextInfoTest.java
+### 4.3 스프링 빈 조회 - 기본
+- ApplicationContextBasicFindTest.java
+### 4.4 스프링 빈 조회 - 동일 타입이 둘 이상
+- ApplicationContextSameBeanFindTest.java
+### 4.7 다양한 설정 형식 지원 - 자바코드, XML
+- XmlAppContext.java
+- src/main/resources/appConfig.xml
+### 4.8 스프링 빈 설정 메타정보 - BeanDefinition
+- BeanDefinitionTest.java
+
+---
+## 5. 싱글톤 컨테이너
+### 5.1 웹 애플리케이션과 싱글톤
+- SingletonTest.java : pureContainer 메소드
+### 5.2 싱글톤패턴
+- SingletonService.java
+- SingletonTest.java : singletonServiceTest 메소드
+### 5.3 싱글톤 컨테이너
+- SingletonTest.java : springContainer 메소드
+### 5.5 @Configuration과 싱글톤
+- SingletonTest.java : configurationDeep 메소드
+
+---
+## 6. 컴포넌트 스캔
+### 6.1 컴포넌트 스캔과 의존관계 자동 주입 시작하기
+- AutoAppConfig.java
+- AutoAppConfigTest.java
+
+
+
+
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,18 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
@@ -22,5 +23,10 @@ public class OrderServiceImpl implements OrderService {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
